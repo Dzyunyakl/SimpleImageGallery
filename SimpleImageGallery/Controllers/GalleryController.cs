7c43d48fd641bb559e2f3b273aa20476b2ae78ee
Lev Dzyunyak <dzyunyakl@gmail.com>
2017-10-28 18:18:15 +0300

    Create service layer, implement ImageService in controller.

--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -11,60 +11,15 @@ namespace SimpleImageGallery.Controllers
 {
     public class GalleryController : Controller
     {
+        private readonly IImage _imageService;
+        
+        public GalleryController(IImage imageService)
+        {
+            _imageService = imageService;
+        }
         public IActionResult Index()
         {
-            var hikingImageTags = new List<ImageTag>();
-            var cityImageTags = new List<ImageTag>();
-
-            var tag1 = new ImageTag()
-            {
-                Description = "Adventure",
-                Id = 0
-            };
-
-            var tag2 = new ImageTag()
-            {
-                Description = "Urban",
-                Id = 1
-            };
-
-            var tag3 = new ImageTag()
-            {
-                Description = "New York",
-                Id = 2
-            };
-
-            hikingImageTags.Add(tag1);
-
-            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
-
-            var imageList = new List<GalleryImage>()
-            {
-                new GalleryImage()
-                {
-                    Title = "Hiking Trip",
-                    Url = "https://static.pexels.com/photos/442559/pexels-photo-442559.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTags
-                },
-
-                new GalleryImage()
-                {
-                    Title = "On The Trail",
-                    Url = "https://static.pexels.com/photos/397096/pexels-photo-397096.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTags
-                },
-
-                new GalleryImage()
-                {
-                    Title = "Downtown",
-                    Url = "https://static.pexels.com/photos/380283/pexels-photo-380283.jpeg",
-                    Created = DateTime.Now,
-                    Tags = cityImageTags
-                }
-            };
-
+            var imageList = _imageService.GetAll();
             var model = new GalleryIndexModel()
             {
                 Images = imageList,
